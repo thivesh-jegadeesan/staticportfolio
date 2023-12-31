980e4f7c52496cdcab69853be4a89900b42149fa
thivesh-jegadeesan <151930863+thivesh-jegadeesan@users.noreply.github.com>
2023-12-31 22:48:59 +0000

    capitalized in home page

--- static/script/home.js
+++ static/script/home.js
@@ -1,130 +1,130 @@
 const element = document.getElementById("devdiv");
 function myFunction() {
    devinterval = setInterval(function() {
-        if (element.innerHTML === "developer...!") {
+        if (element.innerHTML === "Developer...!") {
             element.innerHTML = '';
             clearInterval(devinterval);
             console.log('if');
             freelancer();
 
         } else {
-            if(element.innerHTML == "developer...")
+            if(element.innerHTML == "Developer...")
             {
             element.innerHTML += "!";
             }
-            else if(element.innerHTML == "developer..")
+            else if(element.innerHTML == "Developer..")
             {
             element.innerHTML += ".";
             }
-            else if(element.innerHTML == "developer.")
+            else if(element.innerHTML == "Developer.")
             {
             element.innerHTML += ".";
             }
-            else if(element.innerHTML == "developer")
+            else if(element.innerHTML == "Developer")
             {
             element.innerHTML += ".";
             }
-            else if(element.innerHTML == "develope")
+            else if(element.innerHTML == "Develope")
             {
             element.innerHTML += "r";
             }
-            else if(element.innerHTML == "develop")
+            else if(element.innerHTML == "Develop")
             {
             element.innerHTML += "e";
             }
-            else if(element.innerHTML == "develo")
+            else if(element.innerHTML == "Develo")
             {
             element.innerHTML += "p";
             }
-            else if(element.innerHTML == "devel")
+            else if(element.innerHTML == "Devel")
             {
                 element.innerHTML += "o";
             }
-            else if(element.innerHTML == "deve")
+            else if(element.innerHTML == "Deve")
             {
                 element.innerHTML += "l";
             }
-            else if(element.innerHTML == "dev")
+            else if(element.innerHTML == "Dev")
             {
                 element.innerHTML += "e";
             }
-            else if(element.innerHTML == "de")
+            else if(element.innerHTML == "De")
             {
                 element.innerHTML += "v";
             }
-            else if(element.innerHTML == "d")
+            else if(element.innerHTML == "D")
             {
                 element.innerHTML += "e";
             }
             else{
-                element.innerHTML += "d";
+                element.innerHTML += "D";
             }
         }
     }, 300);
   }
   function freelancer(){
     devinterval = setInterval(function() {
-        if (element.innerHTML === "freelancer...!") {
+        if (element.innerHTML === "Freelancer...!") {
             element.innerHTML = '';
             clearInterval(devinterval);
             Student();
 
         } 
             else {
-                if(element.innerHTML == "freelancer...")
+                if(element.innerHTML == "Freelancer...")
             {
             element.innerHTML += "!";
             }
-            else if(element.innerHTML == "freelancer..")
+            else if(element.innerHTML == "Freelancer..")
             {
             element.innerHTML += ".";
             }
-            else if(element.innerHTML == "freelancer.")
+            else if(element.innerHTML == "Freelancer.")
             {
             element.innerHTML += ".";
             }
-            else if(element.innerHTML == "freelancer")
+            else if(element.innerHTML == "Freelancer")
             {
             element.innerHTML += ".";
             }
-               else if(element.innerHTML == "freelance")
+               else if(element.innerHTML == "Freelance")
                 {
                 element.innerHTML += "r";
                 }
-                else if(element.innerHTML == "freelanc")
+                else if(element.innerHTML == "Freelanc")
                 {
                 element.innerHTML += "e";
                 }
-                else if(element.innerHTML == "freelan")
+                else if(element.innerHTML == "Freelan")
                 {
                 element.innerHTML += "c";
                 }
-                else if(element.innerHTML == "freela")
+                else if(element.innerHTML == "Freela")
                 {
                 element.innerHTML += "n";
                 }
-                else if(element.innerHTML == "freel")
+                else if(element.innerHTML == "Freel")
                 {
                     element.innerHTML += "a";
                 }
-                else if(element.innerHTML == "free")
+                else if(element.innerHTML == "Free")
                 {
                     element.innerHTML += "l";
                 }
-                else if(element.innerHTML == "fre")
+                else if(element.innerHTML == "Fre")
                 {
                     element.innerHTML += "e";
                 }
-                else if(element.innerHTML == "fr")
+                else if(element.innerHTML == "Fr")
                 {
                     element.innerHTML += "e";
                 }
-                else if(element.innerHTML == "f")
+                else if(element.innerHTML == "F")
                 {
                     element.innerHTML += "r";
                 }
                 else{
-                    element.innerHTML += "f";
+                    element.innerHTML += "F";
                 }
             }
 
@@ -132,53 +132,53 @@ function myFunction() {
   }
   function Student(){
     devinterval = setInterval(function() {
-        if (element.innerHTML === "student...!") {
+        if (element.innerHTML === "Student...!") {
             element.innerHTML = '';
             clearInterval(devinterval);
             myFunction();
         } else {
-            if(element.innerHTML == "student...")
+            if(element.innerHTML == "Student...")
             {
             element.innerHTML += "!";
             }
-            else if(element.innerHTML == "student..")
+            else if(element.innerHTML == "Student..")
             {
             element.innerHTML += ".";
             }
-            else if(element.innerHTML == "student.")
+            else if(element.innerHTML == "Student.")
             {
             element.innerHTML += ".";
             }
-            else if(element.innerHTML == "student")
+            else if(element.innerHTML == "Student")
             {
             element.innerHTML += ".";
             }
-               else if(element.innerHTML == "studen")
+               else if(element.innerHTML == "Studen")
                 {
                 element.innerHTML += "t";
                 }
-                else if(element.innerHTML == "stude")
+                else if(element.innerHTML == "Stude")
                 {
                     element.innerHTML += "n";
                 }
-                else if(element.innerHTML == "stud")
+                else if(element.innerHTML == "Stud")
                 {
                     element.innerHTML += "e";
                 }
-                else if(element.innerHTML == "stu")
+                else if(element.innerHTML == "Stu")
                 {
                     element.innerHTML += "d";
                 }
-                else if(element.innerHTML == "st")
+                else if(element.innerHTML == "St")
                 {
                     element.innerHTML += "u";
                 }
-                else if(element.innerHTML == "s")
+                else if(element.innerHTML == "S")
                 {
                     element.innerHTML += "t";
                 }
                 else{
-                    element.innerHTML += "s";
+                    element.innerHTML += "S";
                 }
             }
         
